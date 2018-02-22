586fc58df150d9ba4f355ce7dedb2f05bebfc4ab
Gray Smith <gray.smith00@gmail.com>
2018-02-22 22:21:13 +0000

    add send email functionality so that the password reset token link can be sent to the user's email

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,6 +5,7 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const promisify = require('es6-promisify');
+const mail = require('../handlers/mail');
 
 // LOGIN 
 // ==================================================
@@ -40,12 +41,18 @@ exports.forgot = async (req, res) => {
   await user.save();
   // 3. Send user an email with the reset token
   const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
-  req.flash('success', `You have been emailed a password reset link: ${resetURL}`);
+  await mail.send({
+    user: user, 
+    subject: 'Password Reset',
+    resetURL: resetURL,
+    filename: 'password-reset',
+  });
+  req.flash('success', `You have been emailed a password reset link.`);
   // 4. Redirect to the login page 
   res.redirect('/login');
 };
 
-// FORGOT PASSWORD RESET LINK ACTION
+// FORGOT PASSWORD RESET LINK PAGE
 // =================================================
 exports.reset = async (req, res) => {
   // find the user by the reset token param
@@ -60,27 +67,6 @@ exports.reset = async (req, res) => {
   }
   // if there is a user, show the password form
   res.render('reset', { title: 'Reset your password'});
-};
-
-// MIDDLEWARE TO CHECK IF USER IS LOGGED IN
-// ==================================================
-exports.isLoggedIn = (req, res, next) => {
-  // first check if the user is authenticated, .isAuthenticated method comes with passport
-  if(req.isAuthenticated()) {
-    return next(); // user is authenticated, carry on!
-  }
-  req.flash('error', 'Oops! You gotta be logged in to do that dawgy!');
-  res.redirect('/login');
-};
-
-// MIDDLEWARE TO CHECK IF PASSWORD AND CONFIRM PASSWORD ARE THE SAME
-// ==================================================================
-exports.confirmedPasswords = (req, res, next) => {
-  if (req.body.password === req.body['password-confirm']) {  // you have to use square bracket notation on properties that have dashes
-    return next(); // passwords match carry on
-  }  
-  req.flash('error', 'Passwords do not match!');
-  res.redirect('back');
 };
 
 // RESET PASSWORD UPDATE ACTION
@@ -109,4 +95,28 @@ exports.update = async (req, res) => {
   await req.login(updatedUser);
   req.flash('success', 'Your password has been updated! You are now logged in!');
   res.redirect('/');
+};
+
+
+
+
+// MIDDLEWARE TO CHECK IF USER IS LOGGED IN
+// ==================================================
+exports.isLoggedIn = (req, res, next) => {
+  // first check if the user is authenticated, .isAuthenticated method comes with passport
+  if(req.isAuthenticated()) {
+    return next(); // user is authenticated, carry on!
+  }
+  req.flash('error', 'Oops! You gotta be logged in to do that dawgy!');
+  res.redirect('/login');
+};
+
+// MIDDLEWARE TO CHECK IF PASSWORD AND CONFIRM PASSWORD ARE THE SAME
+// ==================================================================
+exports.confirmedPasswords = (req, res, next) => {
+  if (req.body.password === req.body['password-confirm']) {  // you have to use square bracket notation on properties that have dashes
+    return next(); // passwords match carry on
+  }  
+  req.flash('error', 'Passwords do not match!');
+  res.redirect('back');
 };
